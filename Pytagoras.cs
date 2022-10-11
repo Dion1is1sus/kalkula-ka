2bb6cd751a9bb7e991b344c7c0271d3adf7fa962
Dion1is1sus <113968484+Dion1is1sus@users.noreply.github.com>
2022-10-11 08:48:12 +0200

    Add files via upload

---
+++ Pytagoras.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Enter the First Value ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Second Value ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Third Value ");
+            c = double.Parse(Console.ReadLine());
+            if (a > c)
+            {
+                double tmp = c;
+                c = a;
+                a = tmp;
+
+
+            }
+            if (b > c)
+            {
+                double tmp = c;
+                c = b;
+                b = tmp;
+
+            }
+            if (c == Math.Sqrt(a * a + b * b))
+            {
+                Console.WriteLine("pravouhly");
+            }
+            else
+            {
+                Console.WriteLine("neni pravouhly");
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
+    
+       
